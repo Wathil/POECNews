1963b2087f93785a2c30eadf0760fa4303ef2fa0
Yannick GARCIA <yannick.garcia@icloud.com>
2021-05-16 19:42:36 +0200

    KoaJS OK

---
+++ KoaJS/README.md
@@ -0,0 +1,13 @@
+#
+# npm i koa
+# npm i sequelize
+# npm i mysql2
+# npm i http
+# npm i @koa/router
+# npm i @koa/cors
+# npm i koa-bodyparser@2
+
+# node my-koa-app.js
+#
+# 
+# https://babeljs.io/docs/en/babel-register/
---
+++ KoaJS/controllers/redacteur.controller.js
@@ -0,0 +1,34 @@
+const redacteur = require("../models").redacteur;
+const db = require("../models");
+const Op = db.Sequelize.Op;
+
+module.exports = {
+    async getRedacteurs(ctx) {
+        try {
+            const redacteurCollection = await redacteur.findAll();
+            ctx.status=201;
+            ctx.body = redacteurCollection;
+        }
+        catch (e) {
+            console.log(e);
+            ctx.status=500;
+            ctx.body = e;
+        }
+    },
+    async AddRedacteur(ctx) {
+        try {
+            // ctx.request.body // your POST params
+            // ctx.params // URL params, like :id
+            const obj = ctx.request.body;
+            console.log(obj); // OK
+            const newRedacteur = await redacteur.create(obj);
+            ctx.status=201;
+            ctx.body = newRedacteur;
+        }
+        catch (e) {
+            console.log(e);
+            ctx.status=400;
+            ctx.body = e;
+        }
+    }
+}
