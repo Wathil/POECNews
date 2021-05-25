db1b9c23ea387667fc0d6d0180abf660ee03ab19
Yannick GARCIA <yannick.garcia@icloud.com>
2021-05-25 17:25:41 +0200

    To rebase dev

---
+++ POEC-News/src/app/classes/redacteur.spec.ts
@@ -0,0 +1,7 @@
+import { Redacteur } from './redacteur';
+
+describe('Redacteur', () => {
+  it('should create an instance', () => {
+    expect(new Redacteur()).toBeTruthy();
+  });
+});
--- POEC-News/src/app/pages/gerer-redacteurs/gerer-redacteurs.page.ts
+++ POEC-News/src/app/pages/gerer-redacteurs/gerer-redacteurs.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Redacteur } from 'src/app/classes/redacteur';
 
 @Component({
   selector: 'app-gerer-redacteurs',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GererRedacteursPage implements OnInit {
 
-  constructor() { }
+  hide = true;
+
+  // redacteurs: Array<Redacteur> = new Array<Redacteur>();
+
+  redacteurs: Array<Redacteur> = [{
+    id: 1,
+    redacteurloginname: "test1",
+    email: "test1@test.com",
+    password: "p1"
+  }, {
+    id: 2,
+    redacteurloginname: "test2",
+    email: "test2@test.com",
+    password: "p2"
+  }];
+
+  redacteurForm = this.formBuilder.group({
+    id: [null],
+    redacteurloginname: [''],
+    email: [''],
+    password: ['']
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    console.log(this.redacteurs.length);
+    this.reloadData();
+  }
+
+  reloadData() {
+    console.log("reloadData");
+  }
+
+  ajouterRedacteur() {
+    console.log("ajouterRedacteur");
+    this.hide = !this.hide; 
+  }
+
+  saveRedateur() {
+    console.log("saveRedateur");
+  }
+
+  deleteRedacteur(id: number) {
+    console.log("deleteRedacteur");
   }
 
 }
