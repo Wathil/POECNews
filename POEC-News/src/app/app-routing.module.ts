59b1b9359a9849c6798b5f85507e3eeb26d071d5
Yannick GARCIA <yannick.garcia@icloud.com>
2021-06-21 10:48:59 +0200

    ok

--- POEC-News/src/app/app-routing.module.ts
+++ POEC-News/src/app/app-routing.module.ts
@@ -10,10 +10,6 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  // {
-  //   path: 'folder/:id',
-  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  // },
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
