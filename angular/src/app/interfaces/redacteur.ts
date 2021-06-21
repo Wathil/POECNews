d5825c3800071f8b430ee78b364c876ee376ba7d
Yannick GARCIA <yannick.garcia@icloud.com>
2021-06-21 12:26:53 +0200

    ok

--- angular/src/app/interfaces/redacteur.ts
+++
@@ -1,7 +0,0 @@
-export interface Redacteur {
-    id?: number;
-    userlogin?: string;
-    passwd?: string;
-    penname?: string;
-    accredit?: boolean // Sequelize.INTEGER accredit TINYINT 0 ou 1
-}
